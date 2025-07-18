cb80831d04d25c6818bf91dec24fd864c202dc43
Levi Gleik <leviacedo1@gmail.com>
2025-07-18 20:15:36 -0300

    fix: margin between tab experiences and formation

--- src/app/(home)/components/TabFormation.tsx
+++ src/app/(home)/components/TabFormation.tsx
@@ -7,7 +7,7 @@ export default function TabFormation() {
 	return (
 		<div className="flex flex-col gap-4 p-4">
 			<Card className="gap-0 p-4 bg-primary-50/20" shadow="sm">
-				<CardBody>
+				<CardBody className="gap-1">
 					<span className="text-muted-foreground text-sm">
 						{t("farias-brito.date")}
 					</span>
@@ -15,7 +15,7 @@ export default function TabFormation() {
 					<span className="mb-2 text-muted-foreground text-sm italic">
 						{t("farias-brito.subtitle")}
 					</span>
-					<ul className="mt-2 list-inside list-disc text-muted-foreground text-sm">
+					<ul className="mt-2 list-inside list-disc text-muted-foreground text-sm space-y-2">
 						<li>{t("farias-brito.responsibilities.1")}</li>
 						<li>{t("farias-brito.responsibilities.2")}</li>
 						<li>{t("farias-brito.responsibilities.3")}</li>
@@ -23,7 +23,7 @@ export default function TabFormation() {
 				</CardBody>
 			</Card>
 			<Card className="gap-0 p-4 bg-primary-50/20" shadow="sm">
-				<CardBody>
+				<CardBody className="gap-1">
 					<span className="text-muted-foreground text-sm">
 						{t("icaro.date")}
 					</span>
@@ -31,7 +31,7 @@ export default function TabFormation() {
 					<span className="mb-2 text-muted-foreground text-sm italic">
 						{t("icaro.subtitle")}
 					</span>
-					<ul className="mt-2 list-inside list-disc text-muted-foreground text-sm">
+					<ul className="mt-2 list-inside list-disc text-muted-foreground text-sm space-y-2">
 						<li>{t("icaro.responsibilities.1")}</li>
 						<li>{t("icaro.responsibilities.2")}</li>
 						<li>{t("icaro.responsibilities.3")}</li>
