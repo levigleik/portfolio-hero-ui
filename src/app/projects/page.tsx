7a6d81d7075246804dd4f4e4d35054045a3e6862
Levi Gleik <leviacedo1@gmail.com>
2025-07-21 10:38:06 -0300

    ref: remove unused code

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,7 @@
 import ProjectCard from "@/components/project/ProjectCard";
 import { PROJECT_TECHONOLOGIES } from "@/lib/constants";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-	title: "Projetos",
-};
-
-export default function HomePage() {
+export default function ProjectPage() {
 	return (
 		<div className="mt-4 grid grid-cols-1 items-center justify-center gap-4 font-sans md:grid-cols-2 lg:grid-cols-3 lg:mt-10">
 			{PROJECT_TECHONOLOGIES.map((project) => (
