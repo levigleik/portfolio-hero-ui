2c0e8cc03d5bb27fc927e06241824aa4d3188be6
Levi Gleik <leviacedo1@gmail.com>
2025-07-31 11:34:43 -0300

    ref: update projects url

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,10 @@
 import ProjectCard from "@/components/project/ProjectCard";
-import { PROJECT_TECHONOLOGIES } from "@/lib/constants";
+import { PROJECT_TECHNOLOGIES } from "@/lib/constants";
 
 export default function ProjectPage() {
 	return (
 		<div className="mt-4 grid grid-cols-1 items-center justify-center gap-4 font-sans md:grid-cols-2 lg:grid-cols-3 lg:mt-10">
-			{PROJECT_TECHONOLOGIES.map((project) => (
+			{PROJECT_TECHNOLOGIES.map((project) => (
 				<ProjectCard
 					title={`project-${project.name}.title`}
 					description={`project-${project.name}.description`}
